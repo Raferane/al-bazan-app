d6df2b0effd86092e625698b2cf7e49a2efabee9
Rafe <darkskies6969@gmail.com>
2024-12-20 03:57:52 +0300

    deleted secret info

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,8 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   await Supabase.initialize(
-    url: 'https://ahyrszgfckekdwmzzgww.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFoeXJzemdmY2tla2R3bXp6Z3d3Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczNDYyNzQxNiwiZXhwIjoyMDUwMjAzNDE2fQ.X6nX078JdKyB7FVHmk-RiK-TRvNioItD7kr9NNNlGjo',
+    url: '',
+    anonKey: '',
   );
   runApp(
     const ProviderScope(
